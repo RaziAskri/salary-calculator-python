6d55e1301ac8faea0763851c6abb5ca4461dd863
RaziAskri <65558680+RaziAskri@users.noreply.github.com>
2024-07-22 14:27:56 +0200

    initial commit

---
+++ salary-calculator.py
@@ -0,0 +1,4 @@
+numb_hours = float(input('How many hours did you work last month? '))
+hourly_rate = float(input('What is your hourly rate? '))
+
+print('Last month, you earned : ', float(numb_hours * hourly_rate), 'dollars')
