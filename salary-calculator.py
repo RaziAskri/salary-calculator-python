4fa8145d8886ba6be2ef64bf9c9a533b82417ebb
RaziAskri <65558680+RaziAskri@users.noreply.github.com>
2024-08-29 15:49:05 +0200

    Update salary-calculator.py

--- salary-calculator.py
+++ salary-calculator.py
@@ -1,13 +1,1 @@
-import sys
-
-# Check if the correct number of arguments are provided
-if len(sys.argv) != 3:
-    print("Usage: python salary-calculator.py <hours_worked> <hourly_rate>")
-    sys.exit(1)
-
-# Parse the command-line arguments
-numb_hours = float(sys.argv[1])
-hourly_rate = float(sys.argv[2])
-
-# Calculate and print the earnings
-print('Last month, you earned:', float(numb_hours * hourly_rate), 'dollars')
+print ("Hello World!")
